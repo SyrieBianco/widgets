552756351f9cb2e064773316825266e9bde932ef
syrie bianco <SyrieBianco@gmail.com>
2017-07-07 14:45:32 -0700

    Complete clock widget

--- blank_react_skeleton/frontend/clock.jsx
+++ blank_react_skeleton/frontend/clock.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 
 class Clock extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       time: new Date(),
     };
     this.tick = this.tick.bind(this);
+    this.id = '';
     //add super, move bind(this)
   }
 
   tick() {
     this.setState({time: new Date()});
+    this.render();
   }
 
   componentDidMount() {
-    setInterval(this.tick, 1000);
+    this.id = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.id);
   }
 
   render() {
+    const {time} = this.state;
     return (
-      <h1>Clock Widget</h1>
-      //display time
+      <div className='clock'>
+        <h1>Clock Widget</h1>
+        <h2>{time.toLocaleTimeString()}</h2>
+      </div>
     );
   }
 }
